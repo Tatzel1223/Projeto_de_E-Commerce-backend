308b80ae3abea5f273ecb8542ac6b16d43c0faf6
Tatzel <santosalvesguilherme5@gmail.com>
2026-08-20 01:27:35 +0000

    atualização

---
+++ e-commerceback/src/main/java/br/edu/unifio/e_commerceback/ECommercebackApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.e_commerceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercebackApplication.class, args);
+	}
+
+}
---
+++ e-commerceback/src/test/java/br/edu/unifio/e_commerceback/ECommercebackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.e_commerceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
